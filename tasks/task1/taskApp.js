22678801965cb83bf38f265dd30c9a5007cd743c
dklabdo <sayah.abdo22@gmail.com>
2023-10-21 01:56:38 +0100

    version 3.4

--- tasks/task1/taskApp.js
+++ tasks/task1/taskApp.js
@@ -4,5 +4,12 @@ document.getElementById('form').addEventListener('submit',(e)=>{e.preventDefault
 
 
 function chekTask(val){
-    
+    console.log('test');
+    if(answer.value.includes('village/village_forest/road4/wood_house')){
+        answer.style.border = '3px solid green'
+        alert('Congratulation first task completed')
+    }
+    else{
+        answer.style.border = '3px solid red'
+    }
 }
